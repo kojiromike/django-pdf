d3c838ae92f294412e3687db769b3862d4fc3717
Michael A. Smith <smithm@trueaction.com>
2013-03-19 12:31:49 -0400

    Add packaging and dependency files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name             = "django-pdf",
+	version          = 0.1,
+	url              = "https://github.com/directeur/django-pdf",
+	license          = "GPLv2",
+	description      = "A Django middleware that automatically converts views from html to pdf output.",
+	author           = "Directeur",
+	packages         = find_packages("django_pdf"),
+	package_dir      = {"": "django_pdf"},
+	install_requires = [
+		"setuptools",
+		"django",
+		"xhtml2pdf",
+	],
+)
